b1dd51f500bae8b614393c9ef6049516bd2f9a41
Carl Topham <carl@carl-topham.com>
2020-12-07 18:07:21 +0000

    feat: Process activity Data
    - Total distance

--- src/pages/Run/index.jsx
+++ src/pages/Run/index.jsx
@@ -1,14 +1,117 @@
 import { useSelector } from "react-redux";
-import ActivityDetailTable from "../../components/ActivityDetailTable";
+import moment from "moment";
+import { Fragment, useState } from "react";
+// import ActivityDetailTable from "../../components/ActivityDetailTable";
+const tableHeaderClasses = "px-4 py-2 text-grey-600 dark:bg-green-800 dark:text-grey-100";
+const tableClasses = "border border-green-500 px-4 py-2 text-gray-900 font-medium bg-green-50 dark:bg-green-600 align-top";
+const tableClassesTodo = "border border-grey-800 px-4 py-2 text-gray-900 font-medium text-opacity-25 h-5 align-top dark:bg-grey-700:text-grey-200";
 
+const round = (num) => {
+    return Math.round(num * 100 + Number.EPSILON) / 100;
+};
+
+// eslint-disable-next-line react/prop-types
+const KM = ({ meters }) => {
+    return <>{round(meters / 1000)}</>;
+};
 const RunDetailsPage = () => {
-    const activities = useSelector((state) => state.athlete.activities);
+    const { years, total } = useSelector((state) => state.processedData.activities.run);
+    const currentYear = moment().year();
+    const [tab, setTab] = useState(currentYear);
 
+    console.log(years, total);
     return (
         <div>
             <h1 className="text-5xl font-semibold mt-8 mb-8 text-gray-800">Run</h1>
             <hr />
-            {activities && activities.length ? <ActivityDetailTable activityType="Run" target={3000000} /> : null}
+            {/* {activities && activities.length ? <ActivityDetailTable activityType="Run" target={3000000} /> : null} */}
+
+            <nav className="flex flex-col sm:flex-row">
+                {Object.values(years).map(({ year }) => (
+                    <Fragment key={year}>
+                        <button onClick={() => setTab(year)} type="button" className={`text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none ${tab === year ? "text-blue-500 border-b-2 font-medium border-blue-500" : ""}`}>
+                            {year} {year === currentYear ? "⭐️" : null}
+                        </button>
+                    </Fragment>
+                ))}
+            </nav>
+
+            {Object.values(years)
+                .filter((item) => item.year === tab)
+                .map(({ year, days }) => (
+                    <Fragment key={year}>
+                        <h2 className="text-3xl font-semibold mt-8 mb-4 text-gray-800">{year}</h2>
+                        <table className="w-full mt-2 mb-8">
+                            <thead>
+                                <tr>
+                                    <th className={tableHeaderClasses}>Date</th>
+                                    <th className={tableHeaderClasses}>DoY</th>
+                                    <th className={tableHeaderClasses}>Activities</th>
+                                    <th className={tableHeaderClasses}>Distance</th>
+                                    <th className={tableHeaderClasses}>Elevation</th>
+
+                                    <th className={tableHeaderClasses}>Day distance</th>
+                                    <th className={tableHeaderClasses}>Year cumulative Distance</th>
+                                    <th className={tableHeaderClasses}>Total cumulative Distance</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <>
+                                    {days.map((day) => {
+                                        if (!day.activities.length) {
+                                            return (
+                                                <tr key={day.date}>
+                                                    <td className={tableClassesTodo}>{moment(day.date).format("ddd, Do MMM")}</td>
+                                                    <td className={tableClassesTodo}>{day.day}</td>
+                                                    <td className={tableClassesTodo}>-</td>
+                                                    <td className={tableClassesTodo}>-</td>
+                                                    <td className={tableClassesTodo}>-</td>
+                                                </tr>
+                                            );
+                                        }
+
+                                        return (
+                                            <Fragment key={day.date}>
+                                                {day.activities.map((activity, i) => (
+                                                    <tr key={activity.upload_id}>
+                                                        {i === 0 ? (
+                                                            <>
+                                                                <td className={tableClasses} rowSpan={day.activities.length}>
+                                                                    {moment(day.date).format("ddd, Do MMM")}
+                                                                </td>
+                                                                <td className={tableClasses} rowSpan={day.activities.length}>
+                                                                    {day.day}
+                                                                </td>
+                                                            </>
+                                                        ) : null}
+                                                        <td className={tableClasses}>{activity.name}</td>
+                                                        <td className={tableClasses}>
+                                                            <KM meters={activity.distance} />
+                                                        </td>
+                                                        <td className={tableClasses}>{activity.total_elevation_gain}</td>
+                                                        {i === 0 ? (
+                                                            <>
+                                                                <td className={tableClasses} rowSpan={day.activities.length}>
+                                                                    <KM meters={day.total} />
+                                                                </td>
+                                                                <td className={tableClasses} rowSpan={day.activities.length}>
+                                                                    -
+                                                                </td>
+                                                                <td className={tableClasses} rowSpan={day.activities.length}>
+                                                                    -
+                                                                </td>
+                                                            </>
+                                                        ) : null}
+                                                    </tr>
+                                                ))}
+                                            </Fragment>
+                                        );
+                                    })}
+                                </>
+                            </tbody>
+                        </table>
+                    </Fragment>
+                ))}
         </div>
     );
 };
--- src/redux/sagas/processedData/index.js
+++ src/redux/sagas/processedData/index.js
@@ -1,12 +1,11 @@
 // eslint-disable-next-line import/prefer-default-export
 import { call, put, select, takeEvery } from "redux-saga/effects";
 import moment from "moment";
+import range from "lodash/range";
 
 import eddingtonValue from "../../../utils/eddington";
 
-const processActiities = (activities) => {
-    const activityTypes = ["Run", "Ride"];
-
+const processEddington = (activities, activityTypes) => {
     const data = {};
     activityTypes.forEach((type) => {
         const filteredActivity = activities.filter((activity) => activity.type === type);
@@ -56,7 +55,61 @@ const processActiities = (activities) => {
         };
     });
 
-    return { eddington: data };
+    return data;
+};
+
+const processActivities = (activities, activityTypes) => {
+    const data = {};
+    activityTypes
+        .sort((a, b) => a.upload_id - b.upload_id)
+        .forEach((type) => {
+            const filteredActivity = activities.filter((activity) => activity.type === type);
+
+            const activitiesByYear = {};
+
+            let runningTotalDistance = 0;
+            filteredActivity.forEach((fActivity) => {
+                const year = moment(fActivity.start_date).year();
+                const daysInYear = moment(fActivity.start_date).endOf("year").dayOfYear();
+                // check if year exists,
+                const date = moment(fActivity.start_date);
+                if (typeof activitiesByYear[year] === "undefined") {
+                    const days = range(1, daysInYear + 1);
+                    activitiesByYear[year] = {
+                        year,
+                        total: 0,
+                        days: [...days.map((day) => ({ day, date: moment().year(year).dayOfYear(Number(day)).startOf("day").format(), activities: [], total: 0 }))]
+                    };
+                }
+
+                const dayOYIndex = date.dayOfYear() - 1; // 1- 366 so shift by 1 for 0 based!
+                activitiesByYear[year].days[dayOYIndex].activities.push(fActivity);
+
+                runningTotalDistance += fActivity.distance;
+
+                // Today
+                activitiesByYear[year].days[dayOYIndex].total += fActivity.distance;
+                // Year
+                activitiesByYear[year].total += fActivity.distance;
+            });
+
+            data[type.toLowerCase()] = {
+                years: activitiesByYear,
+                total: runningTotalDistance
+            };
+        });
+
+    return data;
+};
+
+const processData = (activities) => {
+    const activityTypes = ["Run", "Ride"];
+    const eddingtonData = processEddington(activities, activityTypes);
+    const activityData = processActivities(activities, activityTypes);
+    return {
+        eddington: eddingtonData,
+        activities: activityData
+    };
 };
 
 const getAllActivities = (state) => {
@@ -70,7 +123,7 @@ function* updateAthleteActivity() {
 
     const activities = yield select(getAllActivities);
 
-    const data = yield call(processActiities, activities);
+    const data = yield call(processData, activities);
 
     yield put({ type: "PROCESSING_DATA_END", payload: data });
     yield put({ type: "RESET_LOADING_MESSAGE" });
